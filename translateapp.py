3b537e6f670ef0ba8a64285b06fe046fc5aead31
Hamas Akhtar <107211584+HamasAtr@users.noreply.github.com>
2022-12-30 18:32:30 +0500

    First Commit

---
+++ translateapp.py
@@ -0,0 +1,39 @@
+import requests
+
+url = "https://text-translator2.p.rapidapi.com/translate"
+try:
+	
+	with open("contry-name.txt", "r") as f:
+		lines = f.readlines()
+
+	contrydict = {}
+
+	for line in lines:
+		try:
+			parsed = line.split('\t')
+			print(parsed[1])
+		except Exception as e:
+			print("These languages available are")
+
+	inp = input("\nThese languages are available. \nEnter text you want to translate:- ")
+	inp.replace(" ", "%20")
+	FirstLang = input("\nEnter the current language code you that you want to translate:- ")
+	SecondLang = input("\nEnter language code that you want to translate- ")
+except:
+	print("Invalid Language code")
+
+
+payload = f"source_language={FirstLang}&target_language={SecondLang}&text={inp}%3F"
+headers = {
+	"content-type": "application/x-www-form-urlencoded",
+	"X-RapidAPI-Key": "d0542abc35msh5893c920dff09f4p129426jsnf9ab33404316",
+	"X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+datas = response.json()
+try: 
+	b = datas['data']
+	print(f"\n\nTranslated text is:- \n  {b}")
+except:
+	print("Currently Unavailable.")
